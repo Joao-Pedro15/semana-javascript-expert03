722cd359eefecab7ef15c30a58f8c0b5abc476e8
joao pedro <jjoao.monteiro15@gmail.com>
2024-01-04 14:03:43 -0300

    fazendo o factory na index

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -9,13 +9,23 @@ import Events from 'events'
 import TerminalController from "./src/terminalController.js";
 import CliConfig from './src/cliConfig.js';
 import SocketClient from './socket.js';
+import EventManager from './src/eventManager.js';
 
 const [nodePath, filePath, ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
 const componentEmitter = new Events()
 const socketClient = new SocketClient(config)
 await socketClient.initialize()
-// const controller = new TerminalController()
-// await controller.initializeTable(componentEmitter)
+const eventManager = new EventManager({ componentEmitter, socketClient })
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+  roomId: config.room,
+  userName: config.username
+}
+eventManager.joinRoomAndWaitForMessage(data)
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
 
 
