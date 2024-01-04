e9b13e1bf9b7d881d0a654f6b85200561ba1615c
joao pedro <jjoao.monteiro15@gmail.com>
2024-01-03 21:10:32 -0300

    implementando sockerClient no front

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,9 +1,21 @@
+/*
+node index.js \
+  --username joaomonteiro \
+  --room sala01 \
+  --hostUri localhost
+*/
+
 import Events from 'events'
 import TerminalController from "./src/terminalController.js";
+import CliConfig from './src/cliConfig.js';
+import SocketClient from './socket.js';
 
+const [nodePath, filePath, ...commands] = process.argv
+const config = CliConfig.parseArguments(commands)
 const componentEmitter = new Events()
-
-const controller = new TerminalController()
-await controller.initializeTable(componentEmitter)
+const socketClient = new SocketClient(config)
+await socketClient.initialize()
+// const controller = new TerminalController()
+// await controller.initializeTable(componentEmitter)
 
 
